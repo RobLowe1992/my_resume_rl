e824a8cfcd0126f33916b0d8431ade2c716f7ee0
Robert Lowe <RobLowe1992@Yahoo.com>
2017-07-18 21:48:31 -0400

    Initial Commit

---
+++ js/helper.js
@@ -0,0 +1,219 @@
+var HTMLheaderName = '<h1 id="name" class="orange-text">%data%</h1>';
+var HTMLheaderRole = '<span>%data%</span><hr>';
+
+var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLlinkedIn = '<li class="flex-item"><span class="orange-text">linkedin</span><span class="white-text">%data%</span></li>';
+var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+
+var HTMLbioPic = '<img src="%data%" class="biopic">';
+var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
+
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-column"></ul>';
+var HTMLskills = '<span class="white-text">%data%</span>';
+
+var HTMLworkStart = '<div class="work-entry col-lg-6"></div>';
+var HTMLworkEmployer = '<a href="#" class="font-economica"><h3>%data%';
+var HTMLworkTitle = ' - %data%</h3></a>';
+var HTMLworkDates = '<div class="date-text">%data%</div>';
+var HTMLworkLocation = '<div class="location-text">%data%</div>';
+var HTMLworkDescription = '<p><br>%data%</p>';
+
+var HTMLprojectStart = '<div class="project-entry col-lg-4 text-center"></div>';
+var HTMLprojectTitle = '<a href="#" class="font-economica"><h3>%data%</h3></a>';
+var HTMLprojectDates = '<div class="date-text">%data%</div>';
+var HTMLprojectDescription = '<p><br>%data%</p>';
+var HTMLprojectImage = '<img src="%data%" width="300" height="200">';
+
+var HTMLschoolStart = '<div class="education-entry"></div>';
+var HTMLschoolName = '<a class="font-economica" href="#"><h3>%data%';
+var HTMLschoolDegree = '%data%</h3></a>';
+var HTMLschoolDates = '<div class="date-text">%data%</div>';
+var HTMLschoolLocation = '<div class="location-text">%data%</div>';
+var HTMLschoolMajor = '<em><span><br>Major: %data%<span></em>';
+
+var HTMLonlineClasses = '<h3 id="online-classes-tag">Online Classes</h3>';
+var HTMLonlineTitle = '<a class="font-economica" href="#"><h3>%data%';
+var HTMLonlineSchool = ' - %data%</h3></a>';
+var HTMLonlineDates = '<div class="date-text">%data%</div>';
+var HTMLonlineURL = '<br><a class="course-link" href="#">%data%</a>';
+
+var internationalizeButton = '<button id="iBtn">Internationalize</button>';
+var googleMap = '<div id="map"></div>';
+
+
+/*
+The Internationalize Names challenge found in the lesson Flow Control from JavaScript Basics requires you to create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
+*/
+$(document).ready(function() {
+  $('button').click(function() {
+    var $name = $('#name');
+    var iName = inName($name.text()) || function(){};
+    $name.html(iName);
+  });
+});
+
+
+var clickLocations = [];
+
+function logClicks(x,y) {
+  clickLocations.push(
+    {
+      x: x,
+      y: y
+    }
+  );
+  console.log('x location: ' + x + '; y location: ' + y);
+}
+
+$(document).click(function(loc) {
+  // your code goes here!
+});
+
+
+
+/*
+This is the fun part. Here's where we generate the custom Google Map for the website.
+See the documentation below for more details.
+https://developers.google.com/maps/documentation/javascript/reference
+*/
+var map;    // declares a global map variable
+
+
+
+function initializeMap() {
+
+  var locations;
+
+  var mapOptions = {
+    disableDefaultUI: true
+  };
+
+  /*
+  For the map to be displayed, the googleMap var must be
+  appended to #mapDiv in resumeBuilder.js.
+  */
+  map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+
+
+  /*
+  locationFinder() returns an array of every location string from the JSONs
+  written for bio, education, and work.
+  */
+  function locationFinder() {
+
+    // initializes an empty array
+    var locations = [];
+
+    // adds the single location property from bio to the locations array
+    locations.push(bio.contacts.location);
+
+    education.schools.forEach(function(school){
+      locations.push(school.location);
+    });
+
+
+    work.jobs.forEach(function(job){
+      locations.push(job.location);
+    });
+
+    return locations;
+  }
+
+
+  function createMapMarker(placeData) {
+
+    // The next lines save location data from the search result object to local variables
+    var lat = placeData.geometry.location.lat();  // latitude from the place service
+    var lon = placeData.geometry.location.lng();  // longitude from the place service
+    var name = placeData.formatted_address;   // name of the place from the place service
+    var bounds = window.mapBounds;            // current boundaries of the map window
+
+    // marker is an object with additional data about the pin for a single location
+    var marker = new google.maps.Marker({
+      map: map,
+      position: placeData.geometry.location,
+      title: name
+    });
+
+    // infoWindows are the little helper windows that open when you click
+    // or hover over a pin on a map. They usually contain more information
+    // about a location.
+    var infoWindow = new google.maps.InfoWindow({
+      content: name
+    });
+
+
+    google.maps.event.addListener(marker, 'click', function() {
+      infowindow.open(map, marker);
+    });
+
+    // this is where the pin actually gets added to the map.
+    // bounds.extend() takes in a map location object
+    bounds.extend(new google.maps.LatLng(lat, lon));
+    // fit the map to the new marker
+    map.fitBounds(bounds);
+    // center the map
+    map.setCenter(bounds.getCenter());
+  }
+
+  /*
+  callback(results, status) makes sure the search returned results for a location.
+  If so, it creates a new map marker for that location.
+  */
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      createMapMarker(results[0]);
+    }
+  }
+
+  /*
+  pinPoster(locations) takes in the array of locations created by locationFinder()
+  and fires off Google place searches for each location
+  */
+  function pinPoster(locations) {
+
+    // creates a Google place search service object. PlacesService does the work of
+    // actually searching for location data.
+    var service = new google.maps.places.PlacesService(map);
+
+    // Iterates through the array of locations, creates a search object for each location
+      locations.forEach(function(place){
+      // the search request object
+      var request = {
+        query: place
+      };
+
+      // Actually searches the Google Maps API for location data and runs the callback
+      // function with the search results after each search.
+      service.textSearch(request, callback);
+    });
+  }
+
+  // Sets the boundaries of the map based on pin locations
+  window.mapBounds = new google.maps.LatLngBounds();
+
+  // locations is an array of location strings returned from locationFinder()
+  locations = locationFinder();
+
+  // pinPoster(locations) creates pins on the map for each location in
+  // the locations array
+  pinPoster(locations);
+
+}
+
+
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+//Vanilla JS way to listen for resizing of the window
+//and adjust map bounds
+window.addEventListener('resize', function(e) {
+//Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
---
+++ js/resumeBuilder.js
@@ -0,0 +1,293 @@
+
+
+var education = {
+    "schools": [{
+        "name": "Bowie State University",
+        "city": "Bowie, MD",
+        "majors": ["Biology"],
+        "dates": "2012-2014--- ",
+        "url": "www.umd.edu"
+    }, {
+        "name": "University of Maryland",
+        "city": "College Park, MD",
+        "majors": ["Anthropology"],
+        "dates": "2014-2016---: ",
+        "url": "www.bowiestate.edu"
+    }, {
+        "name": "General Assembly",
+        "city": "Washington, D.C.",
+        "majors": ["Web Development Immersive"],
+        "dates": "2017---: ",
+        "url": "https://generalassemb.ly/"
+    }],
+    "onlineCourses": [{
+        "title": "Intro to HTML and CSS",
+        "school": "Udacity",
+        "date": "2016",
+        "url": "https://classroom.udacity.com/courses/ud304/lessons/2617868617/concepts/27713785570923"
+    }, {
+        "title": "Programming Foundations with Python",
+        "school": "Udacity",
+        "date": "2016",
+        "url": "https://classroom.udacity.com/courses/ud036/lessons/990110642/concepts/10079185740923"
+    }, {
+        "title": "Javascript Basics",
+        "school": "Udacity",
+        "date": "2016",
+        "url": "https://classroom.udacity.com/courses/ud804/lessons/1946788554/concepts/25505685350923#"
+    }, {
+        "title": "Object Oriented Javascript",
+        "school": "Udacity",
+        "date": "2017",
+        "url": "https://classroom.udacity.com/courses/ud015/lessons/2593668697/concepts/29558186840923#"
+    }, {
+        "title": "HTML5 Canvas",
+        "school": "Udacity",
+        "date": "2017",
+        "url": "https://classroom.udacity.com/courses/ud292/lessons/3163218691/concepts/31699687910923#"
+    }, {
+        "title": "Intro to AJAX",
+        "school": "Udacity",
+        "date": "2017",
+        "url": "https://classroom.udacity.com/courses/ud110/lessons/3174548544/concepts/31591285700923#"
+    }, {
+        "title": "HTML5 and CSS3",
+        "school": "freeCodeCamp",
+        "date": "2016",
+        "url": "#"
+    }, {
+        "title": "Responsive Design with Bootstrap",
+        "school": "freeCodeCamp",
+        "date": "2016",
+        "url": "#"
+    }, {
+        "title": "jQuery",
+        "school": "freeCodeCamp",
+        "date": "2016",
+        "url": "#"
+    }, {
+        "title": "Basic Front End Development Projects",
+        "school": "freeCodeCamp",
+        "date": "2016",
+        "url": "#"
+    }, {
+        "title": "Basic Javascript",
+        "school": "freeCodeCamp",
+        "date": "2016",
+        "url": "#"
+    }, {
+        "title": "Object Oriented and Functional Programming",
+        "school": "freeCodeCamp",
+        "date": "2017",
+        "url": "#"
+    }
+    ]
+};
+
+var work = {
+    "jobs": [{
+        "employer": "Lowe Systems Inc.",
+        "title": "Logistics",
+        "location": "Upper Marlboro",
+        "dates": "2015-2017",
+        "description": "I was responsible for the scheduling of freight pickups. Our pickups include the eastern seaboard ranging from Maine to Florida. Typical freight loads weigh in at over 100k lbs and often total over 1 million dollars in payload. Other duties include coordinating broker loads with over 15+ drivers."
+    },{
+        "employer": "Farmers Fishers Bakers",
+        "title": "Bartender and Waiter",
+        "location": "Washington, D.C.",
+        "dates": "2012 - 2014",
+        "description": "I was responsible for the service of 150+ customers on a nightly basis. I was also responsible for the drink service for the entire restaurant dealing with over 750+ customers daily. Responsible for over $5000+ sales nightly. Duties also include preparing all mixes, juices, syrups, beers, craft cocktails and stocking the inventory each week.",
+        "url": "www.FarmersFishersBakers.com"
+    }, {
+        "employer": "Lyft",
+        "title": "Driver",
+        "location": "Washington, D.C.",
+        "dates": "2015 - Present",
+        "description": "I was responsible for the transportation and safety of 100+ customers per week throughout the greater Washington D.C. metropolitan area. I maintained a 4.87 approval rating.",
+        "url": "www.Lyft.com"
+    }, {
+        "employer": "Uber",
+        "title": "Driver",
+        "location": "Washington, D.C.",
+        "dates": "2015 - Present",
+        "description": "I was responsible for the transportation and safety of 50+ customers per week throughout the cgreater Washington D.C. metropolitan area I maintained a 4.9 approval rating.",
+        "url": "www.Uber.com"
+    }]
+};
+
+var bio = {
+    "name": "Robert Lowe",
+    "role": "Front-End Web Developer",
+    "welcomeMessage": "Meet the REAL Rob Lowe, Front-end Ninja",
+    "biopic": "images/bioPic.jpg",
+    "contacts": {
+        "mobile": "301-213-1125",
+        "email": "RobLowe1992@yahoo.com",
+        "github": "https://github.com/RobLowe1992",
+        "location": "Washington, D.C.",
+        "linkedIn": "https://www.linkedin.com/in/robert-david-lowe/"
+    },
+    "skills": ["Rails", "Ruby", "Javascript", "Angular", "HTML5", "CSS3", "jQuery", "JSON", "AJAX", "SQL","Node.js", "NPM", "Bootstrap", "Github", "Git"]
+};
+
+var projects = {
+    "projects": [{
+        "title": "Portfolio",
+        "dates": "November, 2016",
+        "description": "This is my personal website. This entire site was created using a combination of all the skills i have acquired up to this point. As I complete projects and add skills to my resume the site is the site is updated to reflect my work and skill set.",
+        "images": "images/proj8.png"
+    }, {
+        "title": "Frogger Style Arcade Game",
+        "dates": "January, 2017",
+        "description": " This game is a frogger clone. The goal of the game is to reach the water near the top of the screen without making contact with the lady bugs. Each time you reach the water the user's score and game level increments by one. If the user makes contact with a lady bug the user's score and game level resets.",
+        "images": "images/proj3.jpg"
+    }, {
+        "title": "TechCrunch Mock Site",
+        "dates":"March, 2017",
+        "description": "This project is meant to display some of my front end development skills. The project resembles the site TechCrunch. It is not an exact copy but very close.",
+        "images": "images/proj6.png"
+    }, {
+        "title": "Random Quote Generator",
+        "dates": "February, 2017",
+        "description": "This project generates random quotes each time the user clicks the submit button. This project utilizes skills such as API handling, event listeners, and quite a bit of css configuration to generate random colors with the new quotes.",
+        "images": "images/proj5.jpg"
+    }, {
+        "title": "Simon",
+        "dates": "May, 2017",
+        "description": "This is a clone of the game Simon. The ojective of the game is to watch a sequence of lights flash and then attempt to match them. Passing the level will cause the number of flashes to increment by one. Failing to pass the level will reset the game at level 1.",
+        "images": "images/proj7.png"
+    }, {
+        "title": "Hangry",
+        "dates": "June, 2017",
+        "description": "This application features restaurants in your local area. It uses a seperate front-end(AngularJS) and back-end(Ruby on Rails). The back-end also utilizes a third party api from Zomato to poulate the blog model.",
+        "images": "images/proj9.png"
+    }]
+};
+
+var formatedName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formatedRole);
+
+var formatedRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formatedName);
+
+var formatedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formatedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGithub);
+
+var formattedLinkedIn = HTMLlinkedIn.replace("%data%", bio.contacts.linkedIn);
+$("#topContacts").append(formattedLinkedIn);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+
+
+
+function displaySkills() {
+      if (bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+
+        for (i=0; i < bio.skills.length; i++) {
+          var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+          $("#skills").append(formattedSkill);
+          }
+        }
+    };
+displaySkills();
+document.getElementById('skills').style.display="inline";
+
+function displayWork() {
+      for (index = 0; index < work.jobs.length; index++) {
+          $("#workExperience .container .row").append(HTMLworkStart);
+
+          var formattedEmployer = HTMLworkEmployer.replace("%data%",work.jobs[index].employer);
+          var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[index].title);
+          var formattedEmployerTitle = formattedEmployer + formattedTitle;
+          $(".work-entry:last").append(formattedEmployerTitle);
+
+          var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[index].location);
+          $(".work-entry:last").append(formattedLocation);
+
+          var formattedDates = HTMLworkDates.replace("%data%", work.jobs[index].dates);
+          $(".work-entry:last").append(formattedDates);
+
+          var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[index].description);
+          $(".work-entry:last").append(formattedDescription);
+        }
+      };
+    displayWork();
+    $(document).click(function(loc) {
+      var x = loc.pageX;
+      var y = loc.pageY;
+
+      logClicks(x,y);
+    });
+
+    function inName(name) {
+      var nameSplit = name.split(" ");
+      nameSplit[1] = nameSplit[1].toUpperCase();
+      nameSplit[0] = nameSplit[0].slice(0,1).toUpperCase() + nameSplit[0].slice(1).toLowerCase();
+      return nameSplit[0] + " " + nameSplit[1];
+    };
+    $("#main").append(internationalizeButton);
+    document.getElementById("iBtn").style.display="none";
+
+projects.display = function() {
+      for (i = 0; i < projects.projects.length; i++) {
+        $("#projects .container .row").append(HTMLprojectStart);
+
+        var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
+        $(".project-entry:last").append(formattedTitle);
+
+        var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
+        $(".project-entry:last").append(formattedDates, '<br>');
+
+        if (projects.projects[i].images.length > 0) {
+            var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[i].images);
+            $(".project-entry:last").append(formattedImage);
+        }
+
+        var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
+        $(".project-entry:last").append(formattedDescription);
+      }
+    };
+projects.display();
+
+function displayEducation() {
+    $("#education").append(HTMLschoolStart);
+
+    for (i = 0; i < education.schools.length; i++) {
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[i].name);
+        var formattedSchoolDegree = HTMLschoolDegree.replace("%data%","");
+        var formattedSchoolNameDegree = formattedSchoolName + formattedSchoolDegree;
+        $(".education-entry:last").append(formattedSchoolNameDegree);
+        var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
+        $(".education-entry:last").append(formattedSchoolMajor);
+        var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
+        $(".education-entry:last").append(formattedSchoolDates);
+        var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[i].city);
+        $(".education-entry:last").append(formattedSchoolLocation);
+    }
+
+    $(".education-entry:last").append(HTMLonlineClasses);
+    for (i = 0; i < education.onlineCourses.length; i++) {
+        var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
+        var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
+        var formattedOnlineTitleSchool = formattedOnlineTitle  +formattedOnlineSchool;
+        $(".education-entry:last").append(formattedOnlineTitleSchool);
+        var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[i].date);
+        $(".education-entry:last").append(formattedOnlineDates);
+        var formattedOnlineURL = HTMLonlineURL.replace("%data%", "Go to Course").replace("#", education.onlineCourses[i].url);
+        $(".education-entry:last").append(formattedOnlineURL);
+    }
+
+};
+displayEducation();
+
+function displayMap() {
+    $("#map").append(googleMap);
+}
